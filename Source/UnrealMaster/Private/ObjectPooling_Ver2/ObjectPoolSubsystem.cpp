01bfc1c2a88328b87bed5e32b2f09c0c143b8623
SIRIDESKTOP\Siri Kim <ksrksr777@gmail.com>
2026-06-12 15:57:53 +0900

    [김시리] 8,9회차 과제 구현 완료

--- Source/UnrealMaster/Private/ObjectPooling_Ver2/ObjectPoolSubsystem.cpp
+++ Source/UnrealMaster/Private/ObjectPooling_Ver2/ObjectPoolSubsystem.cpp
@@ -1,11 +1,15 @@
-/*
 // ObjectPoolSubsystem.cpp
 
 #include "ObjectPooling_Ver2/ObjectPoolSubsystem.h"
 
+#include "ObjectPooling_Ver2/PooledObjectDataAsset.h"
+
 void UObjectPoolSubsystem::Initialize(FSubsystemCollectionBase& Collection)
 {
 	Super::Initialize(Collection);
+	
+	// 레시피들의 모음 - 경로로 가져오기 (하드코딩)
+	PoolCollection = LoadObject<UPoolCollectionDataAsset>(nullptr, TEXT("/Game/ObjectPoolingTest/DA_PoolCollection.DA_PoolCollection"));
 }
 
 void UObjectPoolSubsystem::Deinitialize()
@@ -13,94 +17,107 @@ void UObjectPoolSubsystem::Deinitialize()
 	Super::Deinitialize();
 }
 
+void UObjectPoolSubsystem::SetActorActive(AActor* Actor, bool bActive)
+{
+	if (!Actor) return;
+
+	Actor->SetActorHiddenInGame(!bActive);
+	Actor->SetActorEnableCollision(bActive);
+	Actor->SetActorTickEnabled(bActive);
+}
+
 void UObjectPoolSubsystem::OnWorldBeginPlay(UWorld& InWorld)
 {
 	Super::OnWorldBeginPlay(InWorld);
 	
+	// 월드마다 다시 호출되기 때문에 컨테이너 초기화 필요
+	Pools.Empty();
+	
+	
 	// SpawnActor()를 호출할 때 사용할 추가 옵션들을 담는 구조체
 	FActorSpawnParameters SpawnParams;
 	
+	if (! PoolCollection) return;
+	
 	// 설정된 액터 종류별로 풀 생성
-	for (int32 PoolIndex = 0 ; PoolIndex < PooledObjectData.Num() ; ++PoolIndex)
+	for (UPooledObjectDataAsset* PoolData: PoolCollection->PoolDefinitions)
 	{
-		// 실제 컴포넌트가 들어갈 공간
-		FSingleObjectPool_Ver2 CurrentPoolIndex;
+		if (! PoolData || ! PoolData->ActorTemplate) continue;
 		
-		// 정확한 이름 비교를 위해 '이름 규칙' 정해주기 (규칙 없으면 엔진에서 맘대로 바꾸기도 함)
-		// 이름 정해주기 & 내가 생성한 이름을 최대한 사용해줘
-		SpawnParams.Name = PooledObjectData[PoolIndex].ActorName;
-		SpawnParams.NameMode = FActorSpawnParameters::ESpawnActorNameMode::Requested;
+		// 실제 컴포넌트가 들어갈 공간
+		FSingleObjectPool_Ver2 Pool;
+		
+		// 데이터 에셋을 사용하면서 액터 이름 비교 과정이 빠짐
 		
 		// 여러개가 스폰될 때, 겹치는 상황에서 어떡할건지 고르기
 		SpawnParams.SpawnCollisionHandlingOverride = ESpawnActorCollisionHandlingMethod::AdjustIfPossibleButAlwaysSpawn;
 		
 		// 각 액터를 에디터에서 지정한 개수만큼 만들기
-		for (int32 ObjectIndex = 0 ; ObjectIndex < PooledObjectData[PoolIndex].PoolSize ; ++ObjectIndex)
+		for (int32 ObjectIndex = 0 ; ObjectIndex < PoolData->PoolSize ; ++ObjectIndex)
 		{
 			// 스폰 로직
-			AActor* SpawnedActor = GetWorld()->SpawnActor(
-				PooledObjectData[PoolIndex].ActorTemplate, 
-				&FVector::ZeroVector, &FRotator::ZeroRotator, SpawnParams);
+			AActor* SpawnedActor = GetWorld()->SpawnActor<AActor>(
+				PoolData->ActorTemplate, 
+				FVector::ZeroVector, FRotator::ZeroRotator, SpawnParams);
 			
 			// 월드 아웃라이너에 표시되는 이름 설정
 			SpawnedActor->SetActorLabel(SpawnedActor->GetName());
 			
+			// 컴포넌트 붙이고, 초기화
 			// 붙여줄 컴포넌트 만들고, 언리얼에 기능적 등록 -> 렌더, 틱, 물리 등
 			UPooledObject_Ver2* PoolComp = NewObject<UPooledObject_Ver2>(SpawnedActor);
+			SpawnedActor->AddInstanceComponent(PoolComp);
 			PoolComp->RegisterComponent();
 			
-			// 컴포넌트 붙이고, 초기화
-			SpawnedActor->AddInstanceComponent(PoolComp);
-			PoolComp->Init(this);
+			PoolComp->Init();
 			
-			CurrentPoolIndex.PooledObjects.Add(PoolComp);
+			Pool.PooledObjects.Add(PoolComp);
 			
-			SpawnedActor->SetActorHiddenInGame(true);
-			SpawnedActor->SetActorEnableCollision(false);
-			SpawnedActor->SetActorTickEnabled(false);
+			SetActorActive(SpawnedActor, false);
 		}
 		
 		// ex. Bullet/100개 컴포넌트 뭉치를 저장
-		Pools.Add(CurrentPoolIndex);
+		Pools.Add(PoolData, Pool);
 	}
 }
 
-void UObjectPoolSubsystem::Broadcast_PoolerCleanup()
+void UObjectPoolSubsystem::PoolerCleanup()
 {
-	OnPoolerCleanup_Ver2.Broadcast();
+	for (auto& Pair : Pools)
+	{
+		for (UPooledObject_Ver2* Comp : Pair.Value.PooledObjects)
+		{
+			if (Comp)
+			{
+				Comp->bIsPoolActive = false;
+				
+				AActor* Owner = Comp->GetOwner();
+				if (Owner)
+				{
+					SetActorActive(Owner, false);
+				}
+			}
+		}
+	}
 }
 
 AActor* UObjectPoolSubsystem::GetPooledActor(UPooledObjectDataAsset* PoolData)
 {
-	// 액터 종류 개수
-	int32 PoolCount = PooledObjectData.Num();
-	
-	int32 CurrentPoolIndex = -1;
-	
-	for (int32 i = 0 ; i < PoolCount ; ++i)
-	{
-		// 이름 대조해서 인덱스 찾기
-		if (PooledObjectData[i].ActorName == Name)
-		{
-			CurrentPoolIndex = i;
-			break;
-		}
-	}
-	
-	if (CurrentPoolIndex == -1) return nullptr;
+	FSingleObjectPool_Ver2* Pool = Pools.Find(PoolData);
+	if (Pool == nullptr) return nullptr;
 
 	// 존재한다면, 해당 인덱스 안에 컴포넌트가 몇 개 들어있는지
-	int32 PooledObjectCount = Pools[CurrentPoolIndex].PooledObjects.Num();
+	int32 PooledObjectCount = Pool->PooledObjects.Num();
 	
 	// 사용 대기 중(non-active)인 컴포넌트가 몇번째인지
 	int32 FirstAvailable = -1;
 	
 	for (int32 i = 0 ; i < PooledObjectCount ; ++i)
 	{
-		if (Pools[CurrentPoolIndex].PooledObjects[i] != nullptr)
+		if (Pool->PooledObjects[i] != nullptr)
 		{
 			// 컴포넌트가 active하지 않으면
-			if ( ! Pools[CurrentPoolIndex].PooledObjects[i]->bIsPoolActive)
+			if ( ! Pool->PooledObjects[i]->bIsPoolActive)
 			{
 				FirstAvailable = i;
 				break;
@@ -109,7 +126,7 @@ AActor* UObjectPoolSubsystem::GetPooledActor(UPooledObjectDataAsset* PoolData)
 		else 
 		{
 			// [예외 상황] 해당 슬롯의 컨트롤러가 존재하지 않을 경우, 황급히 만들어주기
-			ReGenerateItem(CurrentPoolIndex, i);
+			ReGenerateItem(PoolData, Pool, i);
 			FirstAvailable = i;
 			break;
 		}
@@ -118,47 +135,40 @@ AActor* UObjectPoolSubsystem::GetPooledActor(UPooledObjectDataAsset* PoolData)
 	// 사용할 수 있는 컴포넌트 인덱스를 얻음
 	if (FirstAvailable >= 0)
 	{
-		UPooledObject_Ver2* ToReturnComp = Pools[CurrentPoolIndex].PooledObjects[FirstAvailable];
+		UPooledObject_Ver2* ToReturnComp = Pool->PooledObjects[FirstAvailable];
 		ToReturnComp->bIsPoolActive = true;
-		
-		// 전체 회수용 델리게이트 바인딩
-		OnPoolerCleanup_Ver2.AddUniqueDynamic(ToReturnComp, &UPooledObject_Ver2::RecycleSelf);
-		
+
 		// 컴포넌트가 붙어있는 액터를 데려와서, 활성화
 		AActor* ToReturnActor = ToReturnComp->GetOwner();
-		ToReturnActor->SetActorHiddenInGame(false);
-		ToReturnActor->SetActorEnableCollision(true);
-		ToReturnActor->SetActorTickEnabled(true);
+		SetActorActive(ToReturnActor, true);
 		
 		return ToReturnActor;
 	}
 	
 	// FirstAvailable == -1인데 bCanGrow가 켜져있다면, 새로 스폰하고 바로 사용하기
-	if (PooledObjectData[CurrentPoolIndex].bCanGrow)
+	if (PoolData->bCanGrow)
 	{
+		if (! PoolData || ! PoolData->ActorTemplate) return nullptr;
+		
 		FActorSpawnParameters SpawnParams;
-		SpawnParams.Name = PooledObjectData[CurrentPoolIndex].ActorName;
-		SpawnParams.NameMode = FActorSpawnParameters::ESpawnActorNameMode::Requested;
 		SpawnParams.SpawnCollisionHandlingOverride = ESpawnActorCollisionHandlingMethod::AdjustIfPossibleButAlwaysSpawn;
 	
-		AActor* SpawnedActor = GetWorld()->SpawnActor(PooledObjectData[CurrentPoolIndex].ActorTemplate, 
-			&FVector::ZeroVector, &FRotator::ZeroRotator, SpawnParams);
+		AActor* SpawnedActor = GetWorld()->SpawnActor<AActor>(PoolData->ActorTemplate, 
+			FVector::ZeroVector, FRotator::ZeroRotator, SpawnParams);
 		SpawnedActor->SetActorLabel(SpawnedActor->GetName());
 	
 		UPooledObject_Ver2* PoolComp = NewObject<UPooledObject_Ver2>(SpawnedActor); // 컴포넌트 생성 & 아우터 지정
+		SpawnedActor->AddInstanceComponent(PoolComp);
 		PoolComp->RegisterComponent();
 	
-		SpawnedActor->AddInstanceComponent(PoolComp);
-		PoolComp->Init(this);
+		PoolComp->Init();
 	
 		// Pools에 새로 만든 컴포넌트 추가
-		Pools[CurrentPoolIndex].PooledObjects.Add(PoolComp);
+		Pool->PooledObjects.Add(PoolComp);
 	
 		PoolComp->bIsPoolActive = true;
+		SetActorActive(SpawnedActor, true);
 		
-		// 전체 회수용 델리게이트 바인딩
-		OnPoolerCleanup_Ver2.AddUniqueDynamic(PoolComp, &UPooledObject_Ver2::RecycleSelf);
-	
 		return SpawnedActor;
 	}
 	else
@@ -177,40 +187,38 @@ void UObjectPoolSubsystem::RecycleActor(AActor* PooledActor)
 
 void UObjectPoolSubsystem::RecyclePooledObject(UPooledObject_Ver2* PoolCompRef)
 {
-	// 바인드 풀기
-	OnPoolerCleanup_Ver2.RemoveDynamic(PoolCompRef, &UPooledObject_Ver2::RecycleSelf);
-	
 	// 비활성화 하기 
 	PoolCompRef->bIsPoolActive = false;
 	
 	AActor* ReturningActor = PoolCompRef->GetOwner();
-	ReturningActor->SetActorHiddenInGame(true);
-	ReturningActor->SetActorEnableCollision(false);
-	ReturningActor->SetActorTickEnabled(false);
+	SetActorActive(ReturningActor, false);
 }
 
-void UObjectPoolSubsystem::ReGenerateItem(int32 PoolIndex, int32 PositionIndex)
+void UObjectPoolSubsystem::ReGenerateItem(UPooledObjectDataAsset* PoolData, FSingleObjectPool_Ver2* Pool, int32 PositionIndex)
 {
 	FActorSpawnParameters SpawnParams;
-	SpawnParams.Name = PooledObjectData[PoolIndex].ActorName;
-	SpawnParams.NameMode = FActorSpawnParameters::ESpawnActorNameMode::Requested;
 	SpawnParams.SpawnCollisionHandlingOverride = ESpawnActorCollisionHandlingMethod::AdjustIfPossibleButAlwaysSpawn;
 	
-	AActor* SpawnedActor = GetWorld()->SpawnActor(PooledObjectData[PoolIndex].ActorTemplate, 
-		&FVector::ZeroVector, &FRotator::ZeroRotator, SpawnParams);
+	AActor* SpawnedActor = GetWorld()->SpawnActor<AActor>(PoolData->ActorTemplate, 
+		FVector::ZeroVector, FRotator::ZeroRotator, SpawnParams);
 	SpawnedActor->SetActorLabel(SpawnedActor->GetName());
 	
 	UPooledObject_Ver2* PoolComp = NewObject<UPooledObject_Ver2>(SpawnedActor);
+	SpawnedActor->AddInstanceComponent(PoolComp);
 	PoolComp->RegisterComponent();
 	
-	SpawnedActor->AddInstanceComponent(PoolComp);
-	PoolComp->Init(this);
+	PoolComp->Init();
 	
 	// 컨트롤러가 없는 자리(PositionIndex)에 정확하게 삽입
-	Pools[PoolIndex].PooledObjects[PositionIndex] = PoolComp;
-	SpawnedActor->SetActorHiddenInGame(true);
-	SpawnedActor->SetActorEnableCollision(false);
-	SpawnedActor->SetActorTickEnabled(false);
+	if (Pool->PooledObjects.IsValidIndex(PositionIndex))
+	{
+		Pool->PooledObjects[PositionIndex] = PoolComp;
+	}
+	else
+	{
+		Pool->PooledObjects.Add(PoolComp);
+	}
+	
+	SetActorActive(SpawnedActor, false);
 }
-*/
 
--- Source/UnrealMaster/Public/ObjectPooling_Ver2/ObjectPoolSubsystem.h
+++ Source/UnrealMaster/Public/ObjectPooling_Ver2/ObjectPoolSubsystem.h
@@ -3,13 +3,11 @@
 #pragma once
 
 #include "CoreMinimal.h"
+#include "PoolCollectionDataAsset.h"
 #include "Subsystems/WorldSubsystem.h"
 #include "PooledObject_Ver2.h"		// 오브젝트에 붙을 컴포넌트 헤더 인클루드
 class UPooledObjectDataAsset;		// 데이터에셋
 #include "ObjectPoolSubsystem.generated.h"
-
-// 풀 매니저가 풀에 들어있는 오브젝트들를 전부 회수하고 싶을 때 사용할 델리게이트
-DECLARE_DYNAMIC_MULTICAST_DELEGATE(FPoolerCleanerSignature_Ver2);
 
 USTRUCT(BlueprintType)
 struct FSingleObjectPool_Ver2
@@ -26,20 +24,20 @@ class UNREALMASTER_API UObjectPoolSubsystem : public UWorldSubsystem
 {
 	GENERATED_BODY()
 	
-/*public:
+public:
 	virtual void Initialize(FSubsystemCollectionBase& Collection) override;
 	virtual void Deinitialize() override;
+	
+	void SetActorActive(AActor* Actor, bool bActive);
 	
 	// AMyObjectPool::BeginPlay() 로직을 여기로
 	virtual void OnWorldBeginPlay(UWorld& InWorld) override;
 	
 public:	
-	// 델리게이트 객체
-	FPoolerCleanerSignature_Ver2 OnPoolerCleanup_Ver2;
 	
 	// 집 나갔던 액터들 전부 회수
 	UFUNCTION(BlueprintCallable)
-	void Broadcast_PoolerCleanup();
+	void PoolerCleanup();
 	
 	// 외부에서 원하는 액터 이름을 대고, PooledObjectData 중 원하는 풀오브젝트 1개를 꺼내가는 함수
 	UFUNCTION(BlueprintCallable)
@@ -53,17 +51,18 @@ public:
 	UFUNCTION(BlueprintCallable)
 	void RecyclePooledObject(UPooledObject_Ver2* PoolCompRef);
 	
-	// 데이터 에셋
-	// 어떤 액터를 몇 개씩 풀링할지 정의하는 설정 정보 - Bullet/100개, Rocket/20개 ...
-	///////////////////////////
+	// 레시피들의 모음
+	TObjectPtr<UPoolCollectionDataAsset> PoolCollection;
+	
+	
 	
 protected:
 	
 	// 실제 생성된 풀링 오브젝트들을 종류별로 관리하는 컨테이너
 	UPROPERTY(VisibleAnywhere, BlueprintReadWrite)
-	TArray<FSingleObjectPool_Ver2> Pools;
+	TMap<TObjectPtr<UPooledObjectDataAsset>, FSingleObjectPool_Ver2> Pools;
 	
 private:
 	// 풀 확장이 허용된 경우 새로운 오브젝트를 생성하여 추가
-	void ReGenerateItem(int32 PoolIndex, int32 PositionIndex);*/
+	void ReGenerateItem(UPooledObjectDataAsset* PoolData, FSingleObjectPool_Ver2* Pool, int32 PositionIndex);
 };
